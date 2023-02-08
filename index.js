22e988fc3c3924ec70d3b06a88d6416e91c33d52
scrviy <91909966+scrviy@users.noreply.github.com>
2023-02-08 13:00:02 -0300

    tipos primitivos

---
+++ index.js
@@ -0,0 +1,15 @@
+// string
+const nome = 'Junior';
+const nome1 = "Junior";
+const nome2 = `Junior`; 
+
+// number
+const num1 = 10; //number
+const num2 = 10.52; //number
+
+//undefined and null -> não aponta para nenhum local na memória.
+let nomeAluno;
+let sobrenomeAluno = null;
+
+// boolean => false || true
+const aprovado = false;
